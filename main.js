9713ef889501af88286e012456ea3586f43b7aaf
Stephen Green <stephengreen1028@gmail.com>
2020-01-26 15:36:46 -0700

    working on assigning variables using ES6, data types, and concatenation/back ticks and template literals.

---
+++ main.js
@@ -0,0 +1,14 @@
+// var, let, const
+/* Use let when you want a value to change
+and use const when you want the value to remain constant*/
+
+// Data types
+// String, Numbers, Boolean, Null, Undefined, Symbol
+
+const name = 'Steve';
+const age = 31;
+
+// Concatenation
+console.log('My name is ' + name + ' and I am ' + age);
+// Template String/Literals
+console.log(`My name is ${name} and I am ${age}`);
