280809a2b2ea989a3fced03c66a3a95e75f7b273
Stephen Green <stephengreen1028@gmail.com>
2020-01-26 16:59:19 -0700

    Finished working on object literals, brushed up on JSON format, created the forLoops.js file to practice looping through objects.

---
+++ forLoops.js
@@ -0,0 +1,17 @@
+const todos1 = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appointment',
+        isCompleted: false
+    },
+];
