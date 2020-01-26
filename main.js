3a72dfbbcaea399c84048c2d8181ff1ca353d60c
Stephen Green <stephengreen1028@gmail.com>
2020-01-26 15:48:44 -0700

    Finished working on assigning var, let, and const. I also worked on properties and methods all using console.log. Created array.js file.

--- main.js
+++ main.js
@@ -5,10 +5,31 @@ and use const when you want the value to remain constant*/
 // Data types
 // String, Numbers, Boolean, Null, Undefined, Symbol
 
-const name = 'Steve';
-const age = 31;
+// const name = 'Steve';
+// const age = 31;
 
-// Concatenation
-console.log('My name is ' + name + ' and I am ' + age);
-// Template String/Literals
-console.log(`My name is ${name} and I am ${age}`);
+// // Concatenation
+// console.log('My name is ' + name + ' and I am ' + age);
+// // Template String/Literals
+// const hello = `My name is ${name} and I am ${age}`;
+// // Passing in a variable to the console.log
+// console.log(hello);
+
+// Properties
+const s = 'Hello world!';
+// Get the length
+console.log(s.length); // <-- Properties don't need (), those are for methods and functions.
+// Change to upper
+console.log(s.toUpperCase());
+// Change to lower case
+console.log(s.toLowerCase());
+// Useing substring
+console.log(s.substring(0, 5)); // <-- Pass in to conditions
+// Add on to each other
+console.log(s.substring(5, 0).toUpperCase());
+// Use the split method
+console.log(s.split(''));
+// Add tags example
+const t = 'technology, computers, it, code';
+// Use the split method to put them into an array
+console.log(t.split(', '));
