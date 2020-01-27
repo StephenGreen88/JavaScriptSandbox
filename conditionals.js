964a1ece7825fe310bc6d3f1e093ada064a03776
Stephen Green <stephengreen1028@gmail.com>
2020-01-26 17:56:50 -0700

    started practicing conditionals.

---
+++ conditionals.js
@@ -0,0 +1,20 @@
+// Set a const variable
+const x = 20;
+
+if (x === 10) {
+    console.log('x is 10');
+} else if (x > 10) {
+    console.log('x is greater than 10');
+} else {
+    console.log('x is less than 10');
+};
+
+// Multiple conditions
+const y = 10
+if (x > 5 || y > 10) {
+    console.log('y is 10');
+} else if (y > 10) {
+    console.log('y is greater than 10');
+} else {
+    console.log('y is less than 10');
+};
