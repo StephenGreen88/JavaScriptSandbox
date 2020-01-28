3c3f441237bb52a7c84af0cb93c2a2110b4a611f
Stephen Green <stephengreen1028@gmail.com>
2020-01-27 18:42:27 -0700

    finishing upconditionals by practicing ternary operators and switch statements.

--- conditionals.js
+++ conditionals.js
@@ -1,20 +1,42 @@
-// Set a const variable
-const x = 20;
+// // // Set a const variable
+//  const x = 20;
 
-if (x === 10) {
-    console.log('x is 10');
-} else if (x > 10) {
-    console.log('x is greater than 10');
-} else {
-    console.log('x is less than 10');
-};
+//  if (x === 10) {
+//      console.log('x is 10');
+//  } else if (x > 10) {
+//      console.log('x is greater than 10');
+//  } else {
+//      console.log('x is less than 10');
+//  };
 
-// Multiple conditions
-const y = 10
-if (x > 5 || y > 10) {
-    console.log('y is 10');
-} else if (y > 10) {
-    console.log('y is greater than 10');
-} else {
-    console.log('y is less than 10');
+// // Multiple conditions
+// const a = 6
+// const b = 11
+
+// if (a > 5 || b > 10) {
+//     console.log('a is more than 5 or b is more than 10');
+// } 
+
+// // Ternary operator <-- Set the value of a variable based on a condition.
+// const c = 10;
+
+// const color = c > 10 ? 'red' : 'blue';
+// // Check to see if it works
+// console.log(color);
+
+// Switches <-- another way to evaluate conditions
+const x = 9;
+
+// const color = x > 10 ? 'red' : 'blue';
+const color = 'green';
+
+switch (color) {
+    case 'red':
+    console.log('color is red');
+    break;
+    case 'blue':
+    console.log('color is blue');
+    break;
+    default:
+        console.log('color is not red or blue');
 };
